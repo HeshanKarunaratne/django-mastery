5fa0655eca7f4f8a6af0b8207ac558db7b4ba08e
HeshanKarunaratne <heshan947102@gmail.com>
2024-05-02 14:20:08 +0530

    completed

--- studybud/studybud/settings.py
+++ studybud/studybud/settings.py
@@ -26,6 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'base.apps.BaseConfig',
+    'rest_framework',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -36,6 +38,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = 'studybud.urls'
@@ -117,3 +120,9 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
+AUTH_USER_MODEL = 'base.User'
+
+MEDIA_ROOT = BASE_DIR / 'static/images'
+MEDIA_URL = '/images/'
